f1c226be9942961e1e50c8a2f332168c2c10c50b
loralridz <noorrida2000@gmail.com>
2020-08-29 08:44:02 -0700

    Added files from folder

---
+++ assets/js/main.js
@@ -0,0 +1,238 @@
+(function($){
+    
+    "use strict";
+    
+    //===== Prealoder
+    
+    $(window).on('load', function(event) {
+        $('.preloader').delay(500).fadeOut(500);
+    });
+    
+    
+    //===== Mobile Menu 
+    
+    $(".navbar-toggler").on('click', function() {
+        $(this).toggleClass('active');
+    });
+    
+    $(".navbar-nav a").on('click', function() {
+        $(".navbar-toggler").removeClass('active');
+    });
+    
+    
+    //===== close navbar-collapse when a  clicked
+    
+    $(".navbar-nav a").on('click', function () {
+        $(".navbar-collapse").removeClass("show");
+    });
+    
+    
+    //===== Sticky
+    
+    $(window).on('scroll', function(event) {    
+        var scroll = $(window).scrollTop();
+        if (scroll < 10) {
+            $(".navigation").removeClass("sticky");
+        } else{
+            $(".navigation").addClass("sticky");
+        }
+    });
+    
+    
+    //===== Section Menu Active
+
+    var scrollLink = $('.page-scroll');
+        // Active link switching
+        $(window).scroll(function() {
+        var scrollbarLocation = $(this).scrollTop();
+
+        scrollLink.each(function() {
+
+          var sectionOffset = $(this.hash).offset().top - 73;
+
+          if ( sectionOffset <= scrollbarLocation ) {
+            $(this).parent().addClass('active');
+            $(this).parent().siblings().removeClass('active');
+          }
+        });
+    });
+    
+    
+    
+    // Parallaxmouse js
+    
+    function parallaxMouse() {
+        if ($('#parallax').length) {
+            var scene = document.getElementById('parallax');
+            var parallax = new Parallax(scene);
+        };
+    };
+    parallaxMouse();
+    
+    
+    //===== Progress Bar
+    
+    if($('.progress-line').length){
+        $('.progress-line').appear(function(){
+            var el = $(this);
+            var percent = el.data('width');
+            $(el).css('width',percent+'%');
+        },{accY: 0});
+    }
+    
+    
+    //===== Counter Up
+    
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1600,
+    });
+    
+    
+    //===== Magnific Popup
+    
+    $('.image-popup').magnificPopup({
+      type: 'image',
+      gallery:{
+        enabled:true
+      }
+    });
+    
+    
+    //===== Back to top
+    
+    // Show or hide the sticky footer button
+    $(window).on('scroll', function(event) {
+        if($(this).scrollTop() > 600){
+            $('.back-to-top').fadeIn(200)
+        } else{
+            $('.back-to-top').fadeOut(200)
+        }
+    });
+    
+    
+    //Animate the scroll to yop
+    $('.back-to-top').on('click', function(event) {
+        event.preventDefault();
+        
+        $('html, body').animate({
+            scrollTop: 0,
+        }, 1500);
+    });
+    
+
+    
+    //===== 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}(jQuery));
+
+
+/*   ANimate header text */
+function init() {
+	wait(1000).then(() => {
+	  clearText();
+	  typeText("Hey there, I'm Rida... \nI'm a ").then(typeLoop);
+	});
+  
+	function typeLoop() {
+	  typeText("Web Developer!")
+		.then(() => wait(2000))
+		.then(() => removeText("Web Developer!"))
+		.then(() => typeText("Web Designer!"))
+		.then(() => wait(2000))
+		.then(() => removeText("Web Designer!"))
+		.then(typeLoop);
+	}
+  }
+  
+ 
+  
+  const elementNode = document.getElementById("type-text");
+  let text = "";
+  
+  function updateNode() {
+	elementNode.innerText = text;
+  }
+  
+  function pushCharacter(character) {
+	text += character;
+	updateNode();
+  }
+  
+  function popCharacter() {
+	text = text.slice(0, text.length - 1);
+	updateNode();
+  }
+  
+  function clearText() {
+	text = "";
+	updateNode();
+  }
+  
+  function wait(time) {
+	if (time === undefined) {
+	  const randomMsInterval = 100 * Math.random();
+	  time = randomMsInterval < 50 ? 10 : randomMsInterval;
+	}
+  
+	return new Promise((resolve) => {
+	  setTimeout(() => {
+		requestAnimationFrame(resolve);
+	  }, time);
+	});
+  }
+  
+  function typeCharacter(character) {
+	return new Promise((resolve) => {
+	  pushCharacter(character);
+	  wait().then(resolve);
+	});
+  }
+  
+  function removeCharacter() {
+	return new Promise((resolve) => {
+	  popCharacter();
+	  wait().then(resolve);
+	});
+  }
+  
+  function typeText(text) {
+	return new Promise((resolve) => {
+	  function type([character, ...text]) {
+		typeCharacter(character).then(() => {
+		  if (text.length) type(text);
+		  else resolve();
+		});
+	  }
+  
+	  type(text);
+	});
+  }
+  
+  function removeText({ length: amount }) {
+	return new Promise((resolve) => {
+	  function remove(count) {
+		removeCharacter().then(() => {
+		  if (count > 1) remove(count - 1);
+		  else resolve();
+		});
+	  }
+  
+	  remove(amount);
+	});
+  }
+  
+  init();
+  
+  
